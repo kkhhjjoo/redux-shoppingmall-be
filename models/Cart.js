4258cb2c6a3b85ba683bb5840b77e1a914ba4a16
hyunjoo <khjoo1203@gmail.com>
2025-08-02 16:04:29 +0900

    클로드 수정

--- models/Cart.js
+++ models/Cart.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const User = require('./User');
 const Product = require('./Product');
 const Schema = mongoose.Schema;
+
 const cartSchema = Schema({
     userId: {
-        type: mongoose.ObjectId, ref:User
+        type: mongoose.ObjectId, 
+        ref: 'User'  // 문자열로 수정
     },
     items: [{
-        productId: {type: mongoose.ObjectId, ref: Product},
+        productId: {type: mongoose.ObjectId, ref: 'Product'}, // 문자열로 수정
         size: {type: String, required: true},
         qty: {type: Number, default: 1, required: true}
     }]
-    
-},
-{timestamps: true})
+}, {timestamps: true});
+
 cartSchema.methods.toJSON = function() {
     const obj = this._doc;
     delete obj.__v;
@@ -22,5 +23,5 @@ cartSchema.methods.toJSON = function() {
     return obj;
 }
 
-const Cart = mongoose.model('Product', cartSchema);
+const Cart = mongoose.model('Cart', cartSchema); // 모델명 수정: 'Product' -> 'Cart'
 module.exports = Cart;
--- models/Order.js
+++ models/Order.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
-const User = require('/User');
+const User = require('./User'); // 경로 수정
 const Product = require('./Product');
 const Schema = mongoose.Schema;
+
 const orderSchema = Schema(
     {
-        userId: {type: mongoose.ObjectId, ref: User, required: true },
+        userId: {type: mongoose.ObjectId, ref: 'User', required: true }, // 문자열로 수정
         status: {type: String, default: 'preparing'},
         totalPrice: {type: Number, required: true, default: 0},
         shipTo: {type: Object, required: true},
         contact: {type: Object, required: true},
+        orderNum: {type: String, required: true}, // orderNum 필드 추가
         items: [
             {
-            productId: {type: mongoose.Object, ref: Product, required: true},
+            productId: {type: mongoose.ObjectId, ref: 'Product', required: true}, // mongoose.Object -> mongoose.ObjectId, 문자열로 수정
             price: {type: Number, required: true},
             qty: {type: Number, required: true, default: 1},
             size: {type: String, required: true},
@@ -20,6 +22,7 @@ const orderSchema = Schema(
     },
     {timestamps: true}
 ); 
+
 orderSchema.methods.toJSON = function() {
     const obj = this._doc;
     delete obj.__v;
