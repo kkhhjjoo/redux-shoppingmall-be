d082c7b278dbc92a787f5b050b64f4ba629350ce
hyunjoo <khjoo1203@gmail.com>
2025-08-02 15:37:36 +0900

    api추가

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userApi = require('./user.api');
 
-router.use('/user', userApi)
+router.use('/user', userApi);
+router.get('/', (req, res) => {
+  res.send('API root is working!');
+});
 
 module.exports = router;
